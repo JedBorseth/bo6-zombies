fd69eafb31886df6ac4885bac1816e8e280038a9
Jed Borseth <jedborseth@Jeds-MacBook-Pro.local>
2025-01-04 15:45:59 -0800

    pwa config may be workin

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,45 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), AstroPWA({
-    /* your pwa options */
-  }), react()],
+  integrations: [
+    tailwind(),
+    AstroPWA({
+      /* your pwa options */
+      workbox: {
+        globPatterns: [
+          "**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}",
+        ],
+        globDirectory: "./dist",
+        navigateFallback: null,
+      },
+      manifest: {
+        name: "COD - Zombies Helper App",
+        short_name: "Zombies Helper",
+        description: "Astro PWA Built by Jed Borseth",
+        theme_color: "#30E130", // Change this to your primary color.
+        background_color: "#ffffff",
+        display: "minimal-ui",
+        icons: [
+          {
+            src: "/favicons/favicon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/favicons/favicon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/favicons/favicon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      registerType: "autoUpdate",
+    }),
+    react(),
+  ],
 });
